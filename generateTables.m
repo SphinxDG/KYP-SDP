9d52fe38270f43b82f68be377c8d6009012b7171
Dennis Gramlich <DeVaDiMa@gmail.com>
2023-03-19 18:34:25 +0100

    Deleted old result files and completed table generation.

--- generateTables.m
+++ generateTables.m
@@ -26,3 +26,32 @@ for jj = 1:length(problem_lists)
 end
 
 computationTimes_table = struct2table(time_struct)
+% table2latex(computationTimes_table,'computationTimes.tex')
+
+
+%% Optimal value table
+
+solvers = {'customSolver','Mosek','SeDuMi','LMILab'};
+problem_lists = {'control_examples','heat_flow_problems','second_order_problems'};
+
+value_struct = struct();
+value_struct.Problem = [];
+
+for ii = 1:length(solvers)
+    solver = solvers{ii};
+    value_struct.(solver) = [];
+    for jj = 1:length(problem_lists)
+        problems = problem_lists{jj};
+        load([solver,'_',problems,'_results.mat']);
+        value_struct.(solver) = [value_struct.(solver);achieved_cost'];
+    end
+end
+
+for jj = 1:length(problem_lists)
+    problems = problem_lists{jj};
+    load([problems,'.mat'])
+    value_struct.Problem = [value_struct.Problem;model_list'];
+end
+
+value_table = struct2table(value_struct)
+% table2latex(value_table,'optimalValues.tex')
